4b3e0880dae5b3e4eea6fd3c75a445abb786eb31
Sanjai K <132209998+pksanjai@users.noreply.github.com>
2025-08-09 11:23:02 +0530

    Update dijkstra_algorithm.java

--- dijkstra_algorithm.java
+++ dijkstra_algorithm.java
@@ -1,58 +1,43 @@
-package tree_and_graph;
 import java.util.*;
-public class dijkstra_algorithm {
-    static ArrayList<ArrayList<ArrayList<Integer>>> constructAdj(int[][] edges,int s){
-        ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<>();
-        for(int i=0;i<=s;i++){
-            adj.add(new ArrayList<>());
+class dijkstra_algorithm{
+    static class pair{
+        int vertex,weight;
+        pair(int vertex,int weight){
+            this.vertex = vertex;
+            this.weight = weight;
         }
-        for(int[] edge:edges){
-            int u = edge[0],v = edge[1],w = edge[2];
-            ArrayList<Integer> e1 = new ArrayList<>();
-            e1.add(v);
-            e1.add(w);
-            adj.get(u).add(e1);
-            ArrayList<Integer> e2 = new ArrayList<>();
-            e2.add(u);
-            e2.add(w);
-            adj.get(v).add(e2);
-        }
-        return adj;
     }
-    static int[] dijkstra(int n,int[][] edges,int s){
-        ArrayList<ArrayList<ArrayList<Integer>>> adj = constructAdj(edges,n);
-        PriorityQueue<ArrayList<Integer>> pq = new PriorityQueue<>(Comparator.comparingInt(a->a.get(1)));
-        int[] dis = new int[n];
-        Arrays.fill(dis,Integer.MAX_VALUE);
-        dis[s] = 0;
-        ArrayList<Integer> start = new ArrayList<>();
-        start.add(s);
-        start.add(0);
-        pq.offer(start);
+    static int[] dijsktra(int start,int v,List<List<pair>> adj){
+        int[] dist = new int[v];
+        Arrays.fill(dist,(int)1e8);
+        dist[start] = 0;
+        PriorityQueue<pair> pq = new PriorityQueue<>(Comparator.comparingInt(a->a.weight));
+        pq.offer(new pair(start,0));
         while(!pq.isEmpty()){
-            ArrayList<Integer> cur = pq.poll();
-            int d = cur.get(1);
-            int u = cur.get(0);
-            for(ArrayList<Integer> neighbour:adj.get(u)){
-                int v = neighbour.get(0);
-                int w = neighbour.get(1);
-                if(dis[v] > dis[u] + w){
-                    dis[v] = dis[u] + w;
-                    ArrayList<Integer> temp = new ArrayList<>();
-                    temp.add(dis[v]);
-                    temp.add(v);
-                    pq.offer(temp);
+            pair current = pq.poll();
+            int ver = current.vertex;
+            for(pair neig:adj.get(ver)){
+                int neighbour = neig.vertex;
+                int nWeight = neig.weight;
+                if(dist[neighbour] > dist[ver] + nWeight){
+                    dist[neighbour] = dist[ver] + nWeight;
+                    pq.offer(new pair(neighbour,dist[neighbour]));
                 }
             }
         }
-        return dis;
+        return dist;
     }
-
-    public static void main(String[] args) {
-        int v = 0;
-        int scr = 0;
-        int[][] edges =  {{0, 1, 4}, {0, 2, 8}, {1, 4, 6}, {2, 3, 2}, {3, 4, 10}};
-        int[] result = dijkstra(v,edges,scr);
-        System.out.println(Arrays.toString(result));
+    
+    public static void main(String[] args){
+        int v = 5;
+        List<List<pair>> adj = new ArrayList<>();
+        for(int i=0;i<v;i++) adj.add(new ArrayList<>());
+        adj.get(0).add(new pair(1,4));
+        adj.get(0).add(new pair(2,8));
+        adj.get(1).add(new pair(4,6));
+        adj.get(1).add(new pair(2,2));
+        adj.get(2).add(new pair(3,2));
+        adj.get(3).add(new pair(4,10));
+        System.out.println(Arrays.toString(dijsktra(0,v,adj)));
     }
 }
